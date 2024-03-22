7c2f6e6f947bfcd7ac4aeca7ec3b7d922ee46f4e
Callie <cjattwood254@gmail.com>
2024-03-22 15:36:47 -0500

    added array to js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 // create an array to store grocery inventory, needs name, quantity, price, and category attributes
-
+const groceryItems = [
+    {name: "Celery", quantity: 12, price: 2, category: "Produce"},
+    {name: "Milk", quantity: 4, price: 4, category: "Dairy"},
+    {name: "Ground Beef", quantity: 2, price: 8, category: "Meat"},
+    {name: "Cupcake", quantity: 12, price: 15, category: "Bakery"}
+];
 // create a function that will alert you an item needs to be restocked when item quantity <= 3
 
 // create an Item class with name, quantity, price, and category attributes
