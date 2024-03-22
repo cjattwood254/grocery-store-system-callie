048e4a1090c486a2426e2d4a031313319e595466
Callie <cjattwood254@gmail.com>
2024-03-22 15:29:21 -0500

    added instructions to js file

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// create an array to store grocery inventory, needs name, quantity, price, and category attributes
+
+// create a function that will alert you an item needs to be restocked when item quantity <= 3
+
+// create an Item class with name, quantity, price, and category attributes
+
+
+
+// use an event listener to make the add to inventory button functional. 
+// it should gather data from the form, create a new Item using form data, append it to the inventory array, and display the new item
+
+// STRETCH GOAL: implement a dynamic table to show inventory
